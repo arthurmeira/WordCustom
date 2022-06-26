9eafd0e028c1eda63d5530e2e812838c21f4df06
Arthur Meria <arthurmeiraprog@gmail.com>
2022-06-26 14:32:08 -0300

    Implementando javascritp

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function red(){
+    let input = document.getElementById('input');
+    let cor = document.getElementsByTagName(style);
+    console.log(innerText.style);
+    input.style.color = 'red';
+}
